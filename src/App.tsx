ce7dcf8adcd790dd8ac914178a87d5e5594f8383
Pedro Soares <pedro.soares@vtex.com.br>
2021-02-27 22:38:40 -0300

    Create basic HTML structure to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import React from 'react'
+
 export function App() {
+  const [showTranslation, setShowTranslation] = React.useState(false)
+
   return (
     <div className="App">
-      Hello World
+      <div>
+        <h2>English Flash Cards</h2>
+        <div>
+          <img 
+            src="https://images.freeimages.com/images/large-previews/1cb/close-squirrel-1381764.jpg" 
+            alt="word"
+            width="200px"
+            height="200px"
+          />
+          <p className="word">Squirrel</p>
+          {showTranslation && <p className="translation">Esquilo</p>}
+          <button onClick={() => setShowTranslation(true)}>See translation</button>
+          <button>Next Word</button>
+        </div>
+      </div>
     </div>
   );
 }
